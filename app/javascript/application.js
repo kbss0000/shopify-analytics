a7e144d5e8047df4c03881cf99557d51b49dc54a
kbssrikar7 <kbsivasrikar@gmail.com>
2025-12-07 14:06:23 +0530

    Fix: Import Chart.bundle in application.js to enable charts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "Chart.bundle"
 import "chartjs-adapter-date-fns"
 import "chartkick"
 
 // React App (loaded conditionally)
-if (document.getElementById('react-dashboard') || 
-    document.getElementById('react-monaco-editor') || 
-    document.getElementById('react-shopify-form')) {
+if (document.getElementById('react-dashboard') ||
+  document.getElementById('react-monaco-editor') ||
+  document.getElementById('react-shopify-form')) {
   import("./react_app.js");
 }
